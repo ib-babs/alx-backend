8bcc82bd5fdb4d242cbab59ab45f6ac1786bb12d
ib-babs <babatundeibrahim426@gmail.com>
2024-05-23 07:51:41 +0100

    Edit 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 '''Class Model - BasicCache class'''
-from basecache import BaseCaching
-from typing import Any
+BaseCaching = __import__('basecache').BaseCaching
 
 
 class BasicCache(BaseCaching):
     '''BasicCache System. BaseCaching subclass'''
 
-    def put(self, key, item) -> None:
-        '''Populate the cache system'''
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
         if key and item:
             self.cache_data.update({key: item})
 
-    def get(self, key) -> Any:
-        '''Get a value from the cache system with a key'''
+    def get(self, key):
+        """ Get an item by key
+        """
         if key is None or self.cache_data.get(key) is None:
             return None
         return self.cache_data.get(key)
